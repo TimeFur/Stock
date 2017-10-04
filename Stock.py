fa176cabfff3137c2669eddfbd9d4afd3af7fc55
M10207432 <M10207432@mail.ntust.edu.tw>
2017-10-04 22:39:26 +0800

    Parse the Stock website
    1. Using the requests to get the raw data
    2. The datatime is used to evalute the previous date
    ("http://www.twse.com.tw/exchangeReport/STOCK_DAY")

--- Stock.py
+++ Stock.py
@@ -4,10 +4,11 @@ import numpy as np
 from numpy import *
 import matplotlib.pyplot as plt
 
-from datetime import datetime
+from datetime import *
 import re
 import requests
 from bs4 import BeautifulSoup
+import copy
 
 from collections import OrderedDict
 from grs import Stock
@@ -28,215 +29,243 @@ import sklearn as sk
         Log Decorate
 ==========================='''
 def showlog(func):
-	def d_f(*argv):
-		result=func(*argv)
-		for i in result:
-			print i
-
-		return result
-	return d_f
+    def d_f(*argv):
+        result=func(*argv)
+        for i in result:
+            print i
+    
+        return result
+    return d_f
 
 class StockObj():
-        def __init__ (self, get_month):
-                print "Stock"
+    def __init__ (self, get_month):
+        print "Stock"
 
-                self.stockdata=OrderedDict()
-                self.today=datetime.now().strftime("%Y/%m/%d")
-                self.get_month=get_month
+        self.stockdata=OrderedDict()
+        self.today=datetime.now().strftime("%Y/%m/%d")
+        self.get_month=get_month
 
-                self.OutputData=OrderedDict()
-                self.InputData=OrderedDict()
+        self.OutputData=OrderedDict()
+        self.InputData=OrderedDict()
 
-                self.machine=OrderedDict()
-
-
-        def showStocknum(self):
-                stock_all=[]
-
-                twse_no = TWSENo()
-                for i in twse_no.all_stock_no:
-                        tmp_dict={}
-                        tmp_dict["id"]=i
-                        tmp_dict["name"]=twse_no.searchbyno(i)[i]
-                        stock_all.append(tmp_dict)
-                        #print i,twse_no.searchbyno(i)[i]
-
-                return stock_all
-
-        def stockGet(self,s_num,month):
-
-                stock = Stock(s_num,month)
-                time_list=[]
-                stock_list=[]
-
-                self.stockdata[s_num]=OrderedDict()
-
-                for i in stock.raw:
-
-                        time_obj=re.sub(r"\d+/",str(int(i[0][:i[0].find('/')])+1911)+"/",i[0],1)
-                        time=datetime.strptime(time_obj,"%Y/%m/%d")
-
-                        self.stockdata[s_num][time_obj]=i[6]
-
-                        time_list.append(time)
-                        stock_list.append(i[6])
-
-                #plt.plot(time_list,stock_list)
-                #plt.show()
-
-                #for stock_num in self.stockdata:
-                #        for  d in self.stockdata[stock_num]:
-                #                print d,self.stockdata[stock_num][d]
+        self.machine=OrderedDict()
 
 
-        def buyStock(self,s_num,date,count):    #("2330","2016/12/12",3)
-                if self.stockdata.has_key(s_num):
-                        pay=float(self.stockdata[s_num][date])*count*1000
-                        print "You have to pay:",pay
+    def showStocknum(self):
+        stock_all=[]
+
+        twse_no = TWSENo()
+        for i in twse_no.all_stock_no:
+            tmp_dict = {}
+            tmp_dict["id"] = i
+            tmp_dict["name"] = twse_no.searchbyno(i)[i]
+            stock_all.append(tmp_dict)
+            #print i,twse_no.searchbyno(i)[i]
+
+        return stock_all
+
+    def stock_url_parse(self, stock_id, month):
+
+        url = "http://www.twse.com.tw/exchangeReport/STOCK_DAY"        
+        str_month = lambda d : ('0' + str(d.month)) if d.month < 10 else str(d.month)
+        dateinfo = datetime.now()
+        result_list = []
+        
+        for i in range(month + 1):
+            #set to the month first data
+            dateinfo = dateinfo - timedelta(days = dateinfo.day - 1)        
+            date = str(dateinfo.year) + str_month(dateinfo) + '01'
+
+            #get the json
+            payload = {'response' : 'json',
+                       'date' : date,
+                       'stockNo' : stock_id}
+            r = requests.get(url, params = payload)
+
+            #get the raw data
+            json_result = r.json()
+            for rawdata in json_result['data']:
+                result_list.append(rawdata)
+            #print json_result['data']
+
+            #move to the previous month
+            dateinfo = dateinfo - timedelta(days = 1)
+        #print result_list
+        return result_list
+    
+    def stockGet(self, s_num, month):
+
+        stocklist = self.stock_url_parse(s_num, month)
+        time_list=[]
+        stock_list=[]
+
+        self.stockdata[s_num]=OrderedDict()
+
+        for i in stocklist:
+
+            time_obj=re.sub(r"\d+/",str(int(i[0][:i[0].find('/')])+1911)+"/",i[0],1)
+            time=datetime.strptime(time_obj,"%Y/%m/%d")
+
+            self.stockdata[s_num][time_obj]=i[6]
+
+            time_list.append(time)
+            stock_list.append(i[6])
+
+        #plt.plot(time_list,stock_list)
+        #plt.show()
+
+        #for stock_num in self.stockdata:
+        #        for  d in self.stockdata[stock_num]:
+        #                print d,self.stockdata[stock_num][d]
+
+
+    def buyStock(self, s_num, date, count):    #("2330","2016/12/12",3)
+        if self.stockdata.has_key(s_num):
+            pay=float(self.stockdata[s_num][date])*count*1000
+            print "You have to pay:",pay
+        else:
+            self.stockGet(s_num,3)
+            pay=float(self.stockdata[s_num][date])*count*1000
+            print "You have to pay:",pay
+
+    def cal_BuyorNotbuy(self, s_num, earn, countday):
+
+        if self.OutputData.has_key(s_num) == False:
+            self.OutputData[s_num]=OrderedDict()
+
+        #--------Check Data retrieve & parameter reasonable
+        if self.stockdata.has_key(s_num) == False:
+            self.stockGet(s_num, self.get_month)
+
+        list_key = self.stockdata[s_num].keys()
+
+        #--------Eval
+        for i, date in enumerate(list_key):
+            Earn_Flag = False
+            if i + countday == len(list_key):
+                break
+
+            for d_index in range(i + 1, i + countday + 1):
+                diff = float(self.stockdata[s_num][list_key[d_index]]) - float(self.stockdata[s_num][date])
+                if diff >= earn:
+                    #print "Earn money at %s, value=%s" % (list_key[d_index], self.stockdata[s_num][list_key[d_index]])
+                    Earn_Flag=True
+
+            if Earn_Flag == True:
+                self.OutputData[s_num][date]=1
+            else:
+                self.OutputData[s_num][date]=0
+
+        return self.OutputData
+
+    def cal_KDBox(self,s_num,calday):
+
+        K=50.0
+        D=50.0
+
+        if self.InputData.has_key(s_num) == False:
+            self.InputData[s_num]=OrderedDict()
+
+        #============Check date
+        if self.stockdata.has_key(s_num)==False:
+            self.stockGet(s_num,self.get_month)
+
+        list_key=self.stockdata[s_num].keys()
+        if(len(list_key)<calday):
+            #yield "Calday is too long, list len=%d" % (len(list_key))
+            return False
+
+        #============Evalaute KD
+        count_day=0
+        RSV=0
+        ex_RSV=0
+
+        while(len(list_key)>calday+count_day):
+            max_stock=0
+            min_stock=0xFFFF
+            avg_v=0.0
+
+            for i in range(calday):
+                v=float(self.stockdata[s_num][list_key[i+count_day]])
+                avg_v = avg_v + v
+                if v>max_stock:
+                    max_stock=v
+                if v<min_stock:
+                    min_stock=v
+
+            avg_v = avg_v/calday
+
+        if(max_stock!=min_stock):
+            RSV=((self.stockdata[s_num][list_key[calday+count_day]]-min_stock)/(max_stock-min_stock))*100.0
+        else:
+            RSV=ex_RSV
+        ex_RSV=RSV
+        now_K=K*(2.0/3.0)+RSV*(1.0/3.0)
+        now_D=D*(2.0/3.0)+now_K*(1.0/3.0)
+
+        K=now_K
+        D=now_D
+
+        #yield "Date:%s CurStock=%f, RSV=%s, K=%f D=%f (min=%f, max=%f)" % (list_key[calday+count_day],self.stockdata[s_num][list_key[calday+count_day]], RSV, now_K, now_D, min_stock, max_stock),
+
+        if self.InputData[s_num].has_key(list_key[calday+count_day]) == False:
+            self.InputData[s_num][list_key[calday+count_day]] = OrderedDict()
+
+        self.InputData[s_num][list_key[calday+count_day]]["CurStock"] = self.stockdata[s_num][list_key[calday+count_day]]
+        self.InputData[s_num][list_key[calday+count_day]]["RSV"] = RSV
+        self.InputData[s_num][list_key[calday+count_day]]["K"] = now_K
+        self.InputData[s_num][list_key[calday+count_day]]["D"] = now_D
+        self.InputData[s_num][list_key[calday+count_day]]["Avg"] = avg_v
+
+        count_day=count_day+1
+
+        return self.InputData
+
+    def cal_RSIBox(self, Stock_id, calday):
+        if self.InputData.has_key(Stock_id) == False:
+            self.InputData[Stock_id]=OrderedDict()
+
+        #============Check date
+        if self.stockdata.has_key(Stock_id) == False:
+            self.stockGet(Stock_id,self.get_month)
+
+        list_key = self.stockdata[Stock_id].keys()
+        if(len(list_key)<calday):
+            print "Calday is too long, list len=%d" % (len(list_key))
+            return False
+
+        #=================
+        count=0
+        while(len(list_key)>calday+count):
+            pos_sum = 0
+            pos_per = 0
+            neg_sum = 0
+            neg_per = 0
+            for i in range(calday):
+                val1=self.stockdata[Stock_id][list_key[i+count]]
+                val2=self.stockdata[Stock_id][list_key[i+count+1]]
+
+                if val2 >= val1:
+                    pos_sum += val2 - val1
                 else:
-                        self.stockGet(s_num,3)
-                        pay=float(self.stockdata[s_num][date])*count*1000
-                        print "You have to pay:",pay
+                    neg_sum += val1 - val2
 
-        def cal_BuyorNotbuy(self,s_num,earn,countday):
+            pos_per = float(pos_sum) / float(calday)
+            neg_per = float(neg_sum) / float(calday)
 
-                if self.OutputData.has_key(s_num) == False:
-                        self.OutputData[s_num]=OrderedDict()
+            if self.InputData[Stock_id].has_key(list_key[calday+count]) == False:
+                self.InputData[Stock_id][list_key[calday+count]] = OrderedDict()
+            if pos_per!=0 or neg_per!=0:
+                RSI = pos_per/(pos_per + neg_per)
+            else:
+                RSI=0
 
-                #--------Check Data retrieve & parameter reasonable
-                if self.stockdata.has_key(s_num)==False:
+            self.InputData[Stock_id][list_key[calday+count]]["Pos_per"]=pos_per
+            self.InputData[Stock_id][list_key[calday+count]]["Neg_per"]=neg_per
+            self.InputData[Stock_id][list_key[calday+count]]["RSI"]=RSI
 
-                        self.stockGet(s_num,self.get_month)
-
-                list_key=self.stockdata[s_num].keys()
-
-                #--------Eval
-                for i,date in enumerate(list_key):
-                        Earn_Flag=False
-                        if i+countday == len(list_key):
-                                break
-
-                        for d_index in range(i+1, i+countday+1):
-                                diff=float(self.stockdata[s_num][list_key[d_index]])-float(self.stockdata[s_num][date])
-                                if diff >= earn:
-                                        #print "Earn money at %s, value=%s" % (list_key[d_index], self.stockdata[s_num][list_key[d_index]])
-                                        Earn_Flag=True
-
-                        if Earn_Flag == True:
-                                self.OutputData[s_num][date]=1
-                        else:
-                                self.OutputData[s_num][date]=0
-
-                return self.OutputData
-
-        def cal_KDBox(self,s_num,calday):
-
-                K=50.0
-                D=50.0
-
-                if self.InputData.has_key(s_num) == False:
-                        self.InputData[s_num]=OrderedDict()
-
-                #============Check date
-                if self.stockdata.has_key(s_num)==False:
-                        self.stockGet(s_num,self.get_month)
-
-                list_key=self.stockdata[s_num].keys()
-                if(len(list_key)<calday):
-                        #yield "Calday is too long, list len=%d" % (len(list_key))
-                        return False
-
-                #============Evalaute KD
-                count_day=0
-                RSV=0
-                ex_RSV=0
-
-                while(len(list_key)>calday+count_day):
-                        max_stock=0
-                        min_stock=0xFFFF
-                        avg_v=0.0
-
-                        for i in range(calday):
-                                v=float(self.stockdata[s_num][list_key[i+count_day]])
-                                avg_v = avg_v + v
-                                if v>max_stock:
-                                        max_stock=v
-                                if v<min_stock:
-                                        min_stock=v
-
-                        avg_v = avg_v/calday
-
-                        if(max_stock!=min_stock):
-                                RSV=((self.stockdata[s_num][list_key[calday+count_day]]-min_stock)/(max_stock-min_stock))*100.0
-                        else:
-                                RSV=ex_RSV
-                        ex_RSV=RSV
-                        now_K=K*(2.0/3.0)+RSV*(1.0/3.0)
-                        now_D=D*(2.0/3.0)+now_K*(1.0/3.0)
-
-                        K=now_K
-                        D=now_D
-
-                        #yield "Date:%s CurStock=%f, RSV=%s, K=%f D=%f (min=%f, max=%f)" % (list_key[calday+count_day],self.stockdata[s_num][list_key[calday+count_day]], RSV, now_K, now_D, min_stock, max_stock),
-
-                        if self.InputData[s_num].has_key(list_key[calday+count_day]) == False:
-                                self.InputData[s_num][list_key[calday+count_day]] = OrderedDict()
-
-                        self.InputData[s_num][list_key[calday+count_day]]["CurStock"] = self.stockdata[s_num][list_key[calday+count_day]]
-                        self.InputData[s_num][list_key[calday+count_day]]["RSV"] = RSV
-                        self.InputData[s_num][list_key[calday+count_day]]["K"] = now_K
-                        self.InputData[s_num][list_key[calday+count_day]]["D"] = now_D
-                        self.InputData[s_num][list_key[calday+count_day]]["Avg"] = avg_v
-
-                        count_day=count_day+1
-
-                return self.InputData
-
-        def cal_RSIBox(self, Stock_id, calday):
-                if self.InputData.has_key(Stock_id) == False:
-                        self.InputData[Stock_id]=OrderedDict()
-
-                #============Check date
-                if self.stockdata.has_key(Stock_id) == False:
-                        self.stockGet(Stock_id,self.get_month)
-
-                list_key = self.stockdata[Stock_id].keys()
-                if(len(list_key)<calday):
-                        print "Calday is too long, list len=%d" % (len(list_key))
-                        return False
-
-                #=================
-                count=0
-                while(len(list_key)>calday+count):
-                        pos_sum = 0
-                        pos_per = 0
-                        neg_sum = 0
-                        neg_per = 0
-                        for i in range(calday):
-                                val1=self.stockdata[Stock_id][list_key[i+count]]
-                                val2=self.stockdata[Stock_id][list_key[i+count+1]]
-
-                                if val2 >= val1:
-                                        pos_sum += val2 - val1
-                                else:
-                                        neg_sum += val1 - val2
-
-                        pos_per = float(pos_sum) / float(calday)
-                        neg_per = float(neg_sum) / float(calday)
-
-                        if self.InputData[Stock_id].has_key(list_key[calday+count]) == False:
-                                self.InputData[Stock_id][list_key[calday+count]] = OrderedDict()
-                        if pos_per!=0 or neg_per!=0:
-                                RSI = pos_per/(pos_per + neg_per)
-                        else:
-                                RSI=0
-
-                        self.InputData[Stock_id][list_key[calday+count]]["Pos_per"]=pos_per
-                        self.InputData[Stock_id][list_key[calday+count]]["Neg_per"]=neg_per
-                        self.InputData[Stock_id][list_key[calday+count]]["RSI"]=RSI
-
-                        count+=1
-                return self.InputData
+            count+=1
+        return self.InputData
 
 class ClassifyObj():
 
@@ -450,18 +479,22 @@ def StockFlow(_trainingmonth, _predictdate, _RSIcaldate, _KDcaldate):
                 result_file.close()
 
 def main():
-        training_month = 12
-        predict_date = "2017/04/12"
-        RSI_caldate = 9
-        KD_caldate = 9
+    training_month = 12
+    predict_date = "2017/04/12"
+    RSI_caldate = 9
+    KD_caldate = 9
 
-        StockFlow(_trainingmonth = training_month,
-                  _predictdate = predict_date,
-                  _RSIcaldate = RSI_caldate,
-                  _KDcaldate = KD_caldate)
-
+    s = StockObj(3)
+    s.stockGet("2330", 5)
+    print s.stockdata
+    '''
+    StockFlow(_trainingmonth = training_month,
+                _predictdate = predict_date,
+                _RSIcaldate = RSI_caldate,
+                _KDcaldate = KD_caldate)
+    '''
 if __name__=="__main__":
-        main()
+    main()
 
 
 
