3b9f8a9a8c188cbffbb4bcc0472698534ffd4711
TimeFur <djs860403@gmail.com>
2018-08-09 01:29:07 +0800

    Radio button

--- Monitor/monitor.py
+++ Monitor/monitor.py
@@ -30,17 +30,32 @@ class ADB_Frame(wx.Frame):
         self.create_screen()
         
     def bind_component(self):
-        #Button
-        self.connect_btn = wx.Button(parent = self.panel, label = "Connect", pos = (500, 500))
-
-        #Bind function
+        #ADB connect Button
+        self.connect_btn = wx.Button(parent = self.panel, label = "Connect", pos = (0, 500))
         self.Bind(wx.EVT_BUTTON, self.Btn_connect_func, self.connect_btn)
+
+        self.connect_rb1 = wx.RadioButton(self.panel, 11, label = 'USB', pos = (100,505), style = wx.RB_GROUP) 
+        self.connect_rb2 = wx.RadioButton(self.panel, 22, label = 'WIFI',pos = (150,505)) 
+        
+        #ADB connect Button
+        self.disconnect_btn = wx.Button(parent = self.panel, label = "Disconnect", pos = (0, 530))
+        self.Bind(wx.EVT_BUTTON, self.Btn_disconnect_func, self.disconnect_btn)
+        self.connect_rb2 = wx.RadioButton(self.panel, 22, label = 'USB', pos = (100,535), style = wx.RB_GROUP)
         
     def Btn_connect_func(self, event):
         print "Click"
+        print self.connect_rb1.GetValue()
+        print self.connect_rb2.GetValue()
+        
         self.counter += 1
         self.label[0][1].SetLabel(str(self.counter))
+        
+    def Btn_disconnect_func(self, event):
+        print "DisClick"
+        self.counter += 1
+        self.label[0][2].SetLabel(str(self.counter))
 
+    #Whole block
     def create_screen(self):
         for i in range(RX):
             tmp = []
