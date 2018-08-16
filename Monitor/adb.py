869bdc142571baf2b77290b9a21d8b5a747933c7
TimeFur <djs860403@gmail.com>
2018-08-17 01:05:32 +0800

    Keep listen the adb monitor

--- Monitor/adb.py
+++ Monitor/adb.py
@@ -1,4 +1,6 @@
 import subprocess
+import os
+import commands
 
 PRE_CMD = 'adb shell "%s"'
 
@@ -7,15 +9,28 @@ PRE_CMD = 'adb shell "%s"'
     -subprocess.PIPE means output type as file
     -shell = True, means the cmd type could be sequence
     it should consider the space & metacharacters
+    <This method would read until the msg done>
 ======================================================='''
 
 def shell(cmd):
     
     command = PRE_CMD % (cmd)
-    #print command
+    print command
     
     p = subprocess.Popen(command, shell = True, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
+    
     return p.stdout.read()
 
+def keep_listen_shell(cmd):
+    p = subprocess.Popen(cmd, shell = True, stdout = subprocess.PIPE)
+    while True:
+        l = p.stdout.readline()
+        if not l:
+            break
+        print l
 
-
+try:
+    #keep_listen_shell("adb shell getevent -r")
+    keep_listen_shell("adb devices")
+except:
+    pass
